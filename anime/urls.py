ec6cb65c5e1255bcb81804e5121e1c745c6bc362
NaiNaniNai <nail02-02@mail.ru>
2023-11-13 11:54:32 +0300

    Add static in page detail of anime

--- anime/urls.py
+++ anime/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [path("", views.AnimeViews.as_view())]
+urlpatterns = [
+    path("", views.AnimeViews.as_view()),
+    path("<int:pk>/", views.AnimeDetailViews.as_view()),
+
+]
