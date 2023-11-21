3501586a735844d5526e3a0f8ff86d1888cc18f9
NaiNaniNai <nail02-02@mail.ru>
2023-11-21 15:02:48 +0300

    Add output category in header, the last anime on index page

---
+++ anime/templatetags/anime_tag.py
@@ -0,0 +1,19 @@
+from django import template
+
+from anime.models import Category, Anime
+
+register = template.Library()
+
+
+@register.simple_tag()
+def get_categories():
+    """Output all categories"""
+    return Category.objects.all()
+
+
+@register.inclusion_tag("tags/last_anime.html")
+def get_last_anime(count):
+    """Output the last anime"""
+    anime = Anime.objects.filter(is_draft=False).order_by("-id")[:count]
+    context = {"last_anime": anime}
+    return context
